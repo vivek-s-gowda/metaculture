1ae77be260caf4fef11bc4143f2e9aac8ba6ce85
Vivek S <sgowdavivek@gmail.com>
2022-09-10 22:50:49 +0530

    Data base integrated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBqQKA_2kTqBx-enGOv7rYvWv6sknOy60Y",
+    authDomain: "metaculture-studio.firebaseapp.com",
+    projectId: "metaculture-studio",
+    storageBucket: "metaculture-studio.appspot.com",
+    messagingSenderId: "203649283072",
+    appId: "1:203649283072:web:3e80b56629bca3304694e5",
+    measurementId: "G-FSY8Z9YB27"
+  }
 };
