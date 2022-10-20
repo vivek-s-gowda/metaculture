460e2a261499a7a6ad0bea2cf52827b49fe095bf
Vivek S <sgowdavivek@gmail.com>
2022-10-20 23:43:42 +0530

    Version 2 home page changes

--- src/app/common-header/common-header.component.ts
+++ src/app/common-header/common-header.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-common-header',
   templateUrl: './common-header.component.html',
-  styleUrls: ['./common-header.component.scss']
+  styleUrls: ['./common-header.component.scss'],
 })
 export class CommonHeaderComponent implements OnInit {
   cartCount: number = 0;
-  constructor(private cartService: CartService) { }
-
-  ngOnInit(): void {
-    this.cartService.getCartCount.subscribe((value:number) =>{ 
-      this.cartCount = value;
-    })
+  path: string = '/';
+  constructor(private cartService: CartService, private router: Router) {
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.path = event.url;
+      });
   }
 
-
+  ngOnInit(): void {
+    this.cartService.getCartCount.subscribe((value: number) => {
+      this.cartCount = value;
+    });
+  }
 }
